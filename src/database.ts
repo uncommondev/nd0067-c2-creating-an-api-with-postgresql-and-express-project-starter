957a158349a9752033c94fbe48a4c3e3260e5d04
warp9training <uncommondev344@gmail.com>
2021-11-23 23:36:08 +0000

    Placeholder comment
    Added a comment with the test commands to "save" them in case of a rollback

--- src/database.ts
+++ src/database.ts
@@ -14,6 +14,9 @@ POSTGRES_TEST_PASSWORD,
 ENV
 } = process.env
 
+// "test": "ENV=test && db-migrate --en~v test reset && db-migrate --env test up && jasmine-ts && db-migrate --env test reset",
+// "test-dev": "ENV=dev && db-migrate --env dev reset && db-migrate --env dev up && jasmine-ts && db-migrate --env dev reset",
+
 let client: Pool = new Pool({
         host: POSTGRES_HOST,
         database: ENV === "dev" ? POSTGRES_DB : POSTGRES_TEST_DB,
